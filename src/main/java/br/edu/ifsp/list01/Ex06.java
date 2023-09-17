d5a87bf6a055d0a55fd7698a41fde1a5e6d378b4
listas-nivelamento-main <fernando.candido@aluno.ifsp.edu.br>
2023-09-17 01:01:16 -0300

    gi commit-m ok

--- src/main/java/br/edu/ifsp/list01/Ex06.java
+++ src/main/java/br/edu/ifsp/list01/Ex06.java
@@ -1,5 +1,8 @@
 package br.edu.ifsp.list01;
 
+import java.util.Map;
+import java.util.Scanner;
+
 /*
     Sabe-se que uma lata de tinta tem um custo C e é capaz de pintar uma área de M metros quadrados.
     Faça um programa que leia a largura L, a altura A de uma parede, o valor C de uma lata de tinta e
@@ -23,13 +26,40 @@ public class Ex06 {
 
     public static void main(String[] args) {
         //Leia o input
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("largura: ");
+        double largura = scanner.nextDouble();
+
+        System.out.printf("Altura: ");
+        double altura =  scanner.nextDouble();
+
+        System.out.printf("valor da lata:");
+        double c = scanner.nextDouble();
+
+        System.out.printf("rendimento: ");
+         double m = scanner.nextDouble();
+
+
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        Ex06 ex = new Ex06();
+
         //Escreva o resultado da chamada do método compute() aqui
+        String resut ="";
+
+         resut =  ex.compute(largura,altura,c,m);
+        System.out.println(resut);
+
     }
 
-    String compute(double L, double a, double c, double m) {
-        String output = null;
-        //put your logic here
-        return output;
+    String compute(double l, double a, double c, double m) {
+       //  int  lata = (int) c;
+          double area = l * a;
+          double qtdeTinta =area /m ;
+          double custoTotal  = c * Math.ceil(qtdeTinta);
+        String custoTotalString = String.format("%.2f", custoTotal).replace(",", ".");
+
+         return String.format("%.0fgit  %.2f",  Math.ceil(qtdeTinta) ,custoTotal).replace(",",".") ;
+
+       // return Math.ceil(qtdeTinta) + " custo total " + custoTotalString;
     }
 }
